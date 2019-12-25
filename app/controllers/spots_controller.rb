526cee13676148fa539f25c5a8af663f693c28d3
misaki333 <misakiumenishi@gmail.com>
2019-12-25 06:41:38 +0000

    Fix CSS

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -7,5 +7,6 @@ class SpotsController < ApplicationController
 	def show
 		@spot = Spot.find(params[:id])
 		@posts = Post.where(spot_id: @spot.id)
+    post_images = PostImage.joins(:@post).where(post_id: {spot_id: @spot.id})
 	end
 end
