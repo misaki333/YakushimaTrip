c94554f3ec96a659412facae9fc903b060a23922
misaki333 <misakiumenishi@gmail.com>
2019-12-26 21:40:10 +0000

    Fix something

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,4 +1,5 @@
 class Destination < ApplicationRecord
   belongs_to :plan
   belongs_to :category
+  belongs_to :spot
 end
--- db/migrate/20191208082943_create_destinations.rb
+++ db/migrate/20191208082943_create_destinations.rb
@@ -3,7 +3,6 @@ class CreateDestinations < ActiveRecord::Migration[5.2]
     create_table :destinations do |t|
       t.integer :plan_id
       t.time :time
-      t.string :to_go
       t.string :to_do
 
       t.timestamps
